debf42ed1ec01062e0e2f4c30b9df9ef4217f966
Mushus <mushus.wynd@gmail.com>
2018-06-19 23:21:28 +0900

    tsv->txt

--- batch/download.sh
+++ batch/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q https://raw.githubusercontent.com/Mushus/piyopoke/master/config/calender.tsv -O ~/piyopoke/calender.tsv
-wget -q https://raw.githubusercontent.com/Mushus/piyopoke/master/config/pokelist.txt -O ~/piyopoke/pokelist.tsv
+wget -q https://raw.githubusercontent.com/Mushus/piyopoke/master/config/pokelist.txt -O ~/piyopoke/pokelist.txt
